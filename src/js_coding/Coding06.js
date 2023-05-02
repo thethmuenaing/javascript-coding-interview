e902960ac11035d54cb56d87c767ae4d3d13648c
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-05-02 20:42:09 +0630

    find the missing number in integer array of 1 to 10

---
+++ src/js_coding/Coding06.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Coding06 = () => {
+	return (
+		<div>
+			<h1>
+				How to find the missing number in a given integer array of 1 to 10
+			</h1>
+		</div>
+	);
+};
+
+export default Coding06;
